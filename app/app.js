4918a87584cb095116a8b41a8a5b784f82845f46
JD-Neto <jdneto84@gmail.com>
2024-01-24 21:30:56 +0100

    add security headers

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const app = express();
 const port = 3000;
+
+app.disable('x-powered-by');
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
