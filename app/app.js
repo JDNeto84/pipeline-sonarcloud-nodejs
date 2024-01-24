4fbdb029d3519fb4b305ba581ac6ec96e6a43968
JD-Neto <jdneto84@gmail.com>
2024-01-24 23:26:31 +0100

    failure simulation sonar

--- app/app.js
+++ app/app.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/unsafe', (req, res) => {
+  const userProvidedData = req.query.data;
+  res.send(`You provided: ${userProvidedData}`);
+});
+
 if (require.main === module) {
   app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
