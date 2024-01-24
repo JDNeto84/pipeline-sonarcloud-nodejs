bcc063985bf2aba51d8510b3d88ad9e3a64d22cd
JD-Neto <jdneto84@gmail.com>
2024-01-24 12:49:32 +0100

    upd test.js

--- app/app.test.js
+++ app/app.test.js
@@ -2,6 +2,16 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('Test the app Node.js', () => {
+  let server;
+
+  beforeAll(() => {
+    server = app.listen(3000);
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
   it('Should return the message "Hello, World!"', async () => {
     const response = await request(app).get('/');
     expect(response.text).toBe('Hello, World!');
